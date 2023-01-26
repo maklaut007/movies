33e311795323aca4cd9d5952b3c2a428ce1bc995
Maksym Zinchenko <maklaut98@gmail.com>
2023-01-25 21:52:54 -0600

    Mongodb connected

---
+++ src/test/java/com/cinema/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
